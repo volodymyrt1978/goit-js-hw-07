ce8fd261ecf947582f58ebc741d94953c3c30382
volodymyrt1978 <volodymyr.telyovych@gmail.com>
2022-07-30 18:51:12 +0300

    markup for gallery in task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryImages = [];
+
+galleryItems
+    .forEach((item) => {
+        const divWithImage = document.createElement('div');
+        divWithImage.classList.add('gallery__item');
+
+        const imageLink = document.createElement('a');
+        imageLink.classList.add('gallery__link');
+        imageLink.href = item.original;
+        divWithImage.appendChild(imageLink);
+
+        const image = document.createElement('img');
+        image.classList.add('gallery__image');
+        image.src = item.preview;
+        image.setAttribute('data-source', `${item.original}`);
+        image.alt = item.description;
+        imageLink.appendChild(image);
+
+        galleryImages.push(divWithImage);
+})
+const gallery = document.querySelector('.gallery');
+
+gallery.append(...galleryImages);
+
+
+
+
+
 console.log(galleryItems);
