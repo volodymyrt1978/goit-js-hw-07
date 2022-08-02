1507db42d472eccd5945cafce77ce8040a7eb1d6
volodymyrt1978 <volodymyr.telyovych@gmail.com>
2022-08-02 17:48:11 +0300

    02-lightbox markup

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryLightboxImages = [];
+
+galleryItems
+    .forEach((item) => {
+                
+        const imageLink = document.createElement('a');
+        imageLink.classList.add('gallery__item');
+        imageLink.href = item.original;
+                
+        const image = document.createElement('img');
+        image.classList.add('gallery__image');
+        image.src = item.preview;
+        image.alt = item.description;
+        imageLink.appendChild(image);
+        
+        galleryLightboxImages.push(imageLink);
+    })
+
+const gallery = document.querySelector('.gallery');
+    
+gallery.append(...galleryLightboxImages);
+
+
+
 console.log(galleryItems);
